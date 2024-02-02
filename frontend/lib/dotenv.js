1689ee1d1e93421b31e4d5956805a1c273ca23fa
Moritz Schepp <moritz.schepp@gmail.com>
2024-02-01 19:58:30 -0800

    fixes issues reported via mail

--- frontend/lib/dotenv.js
+++ frontend/lib/dotenv.js
@@ -1,14 +1,9 @@
-import {i18n} from '@wendig/lib'
-import {baseUrl} from './util'
-
-const APP_URL = {
-  en: process.env.APP_URL_EN,
-  de: process.env.APP_URL_DE,
-  fr: process.env.APP_URL_FR
-}
-
 const config = {
-  APP_URL: APP_URL[i18n.locale],
+  APP_URLS: {
+    en: process.env.APP_URL_EN,
+    de: process.env.APP_URL_DE,
+    fr: process.env.APP_URL_FR
+  },
   STATIC_URL: process.env.STATIC_URL,
   MIRADOR_URL: process.env.MIRADOR_URL
 }
